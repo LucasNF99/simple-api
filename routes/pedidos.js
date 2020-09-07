2379eb29f2bdcd5f004c2240446886e08ee31eb5
Lucas Natalicio Ferreira <lucasnf99@gmail.com>
2020-09-07 12:12:41 -0300

    Rota de pedidos

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -3,37 +3,28 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.status(200).send({
-    mensagem: 'Usando o get dentro de pedidos'
+    mensagem: 'Retorna os pedidos'
   })
 });
 
 router.post('/', (req, res, next) => {
   res.status(201).send({
-    mensagem: 'Usando o post dentro de pedidos'
+    mensagem: 'Cria um pedido'
   })
 });
 
 router.get('/:id_pedido', (req, res, next) => {
   const id = req.params.id_pedido
-  if(id === 'especial'){
-    res.status(200).send({
-      mensagem: 'Você descobriu o id especial',
-      id: id
-    })
-  } else {
-    res.status(200).send({
-      mensagem: 'Você passou um ID',
-      id: id
-    })
-  }
-
+  res.status(200).send({
+    mensagem: 'Detalhes do pedido',
+    id: id
+  })
 });
-
 
 
 router.delete('/', (req, res, next) => {
   res.status(201).send({
-    mensagem: 'Usando o delete dentro de pedidos'
+    mensagem: 'Pedido excluido'
   })
 });
 
